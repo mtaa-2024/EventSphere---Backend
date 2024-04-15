83878d863dfe33a5479e2374fd698c0a691a19dc
Matúš Políček <91195468+MatusPolicek@users.noreply.github.com>
2024-04-15 19:44:36 +0200

    updated version

--- index.js
+++ index.js
@@ -10,20 +10,18 @@ app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`);
 });
 
-app.get('/user', db.getUser)
 // Event endpoint
-app.get("/event", db.getEvent);  // Params = (id: int), Returns = Event information + performers + comments
-app.get('/event/comments', db.getUpdatedComments); // Params = (id: int), Returns = Event comments
-// Updating and creating event
-app.post('/create/event', db.createEvent); // Params = (user_id, title, description, location, closing_date, performers), Returns true if creation was successful
-app.post('/update/event', db.updateEvent); // Params = (id: int, title, description, location, date, performers) [optional], Returns true
-//app.delete('/event', db.deleteEvent); // Params = (id: int), Returns true if event was successfully deleted
-// Home screen
-app.get('/upcoming', db.getUpcoming); // Getting upcoming events
-app.get('/attending', db.getAttending); // Getting attending events Params = (user_id: int)
-app.get('/categories', db.filterByCategory); // Filter events by category
-app.get('/event/search', db.searchEvent); // Search for event by string
-// User management
+app.get("/event", db.getEvent);
+app.get('/event/comments', db.getUpdatedComments);
+
+app.post('/create/event', db.createEvent);
+app.post('/update/event', db.updateEvent);
+
+app.get('/upcoming', db.getUpcoming);
+app.get('/attending', db.getAttending);
+app.get('/categories', db.filterByCategory);
+app.get('/event/search', db.searchEvent);
+
 app.get('/login',db.getLoginData);
 app.post('/register',db.createNewUser)
 app.post('/user/edit', db.editUserProfile)
